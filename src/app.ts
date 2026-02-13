cd6741262c7c68401d02179eb202be43b34b857d
Sameer-0077 <sameeryadav0077@gmail.com>
2026-02-14 01:02:58 +0530

    fix: correct import path for healthz route

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express, { Application, Request, Response } from "express";
-import healthzRoute from "./routes/healthz";
+import healthzRoute from "./routes/healthz.routes";
+import pastesRoutes from "./routes/pastes.routes";
 
 const app: Application = express();
 
 app.use(express.json());
 
+app.use("/api", pastesRoutes);
+app.use("/api", healthzRoute);
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Pastebin is running  🚀");
 });
-
-app.use("/api", healthzRoute);
 
 export default app;
