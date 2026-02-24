75e7a19d22d7618a826ea8a0c82d59520ce71f75
Sameer-0077 <sameeryadav0077@gmail.com>
2026-02-24 22:38:34 +0530

    feat: add route for retrieving pastes by ID

--- src/routes/pastes.routes.ts
+++ src/routes/pastes.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { handleCreatePaste } from "../controllers/pastes.controllers";
+import { getPaste, handleCreatePaste } from "../controllers/pastes.controllers";
 import { pasteValidation } from "../middlewares/pastesValidation.middlewares";
 
 const router = Router();
 
 router.post("/pastes", pasteValidation, handleCreatePaste);
+router.get("/pastes/:id", getPaste);
 
 export default router;
